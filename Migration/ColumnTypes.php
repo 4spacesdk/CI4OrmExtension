6996e1e15e88b0a60474e9ab650b523c6034d767
Martin-4Spaces <faaax@msn.com>
2019-06-27 09:28:29 +0200

    Migration tools

---
+++ Migration/ColumnTypes.php
@@ -0,0 +1,23 @@
+<?php namespace OrmExtension\Migration;
+
+class ColumnTypes {
+    const INT           = 'INT NOT NULL';
+    const INT_NULL      = 'INT';
+    const DECIMAL       = 'DECIMAL(10,4) NOT NULL';
+    const FLOAT         = 'FLOAT NOT NULL';
+    const DOUBLE        = 'DOUBLE NOT NULL';
+    const VARCHAR_2047  = 'VARCHAR(2047) NOT NULL';
+    const VARCHAR_1023  = 'VARCHAR(1023) NOT NULL';
+    const VARCHAR_511   = 'VARCHAR(511) NOT NULL';
+    const VARCHAR_255   = 'VARCHAR(255) NOT NULL';
+    const VARCHAR_127   = 'VARCHAR(127) NOT NULL';
+    const VARCHAR_63    = 'VARCHAR(63) NOT NULL';
+    const VARCHAR_27    = 'VARCHAR(27) NOT NULL';
+    const CREATED       = 'DATETIME';
+    const DATETIME      = 'DATETIME';
+    const TIME          = 'TIME NOT NULL';
+    const TEXT          = 'TEXT NOT NULL';
+    const MEDIUMTEXT    = 'MEDIUMTEXT NOT NULL';
+    const BOOL_0        = 'BOOLEAN NOT NULL DEFAULT FALSE';
+    const BOOL_1        = 'BOOLEAN NOT NULL DEFAULT TRUE';
+}
---
+++ Migration/Table.php
@@ -0,0 +1,131 @@
+<?php namespace OrmExtension\Migration;
+
+use CodeIgniter\Database\BaseConnection;
+use CodeIgniter\Database\Forge;
+use Config\Database;
+use DebugTool\Data;
+use OrmExtension\DataMapper\ModelDefinitionCache;
+
+/**
+ * Class Table
+ * @package App\Extensions
+ * @property string $name
+ * @property BaseConnection $db
+ * @property Forge $forge
+ */
+class Table {
+
+    public static function init($name, $group = null) {
+        if(!$group) {
+            $group = (new Database())->defaultGroup;
+        }
+
+        $table = new \App\Extensions\Table();
+        $table->name = $name;
+        $table->db = Database::connect($group);
+        $table->forge = Database::forge($group);
+        $table->create();
+        return $table;
+    }
+
+    public function create() {
+        $sql = "CREATE TABLE IF NOT EXISTS `$this->name` (
+                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                  PRIMARY KEY (`id`)
+                ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;";
+        $this->db->query($sql);
+        return $this;
+    }
+
+    public function hasColumn($name) {
+        $db = $this->db->getDatabase();
+        $sql = "SELECT count(*) as count FROM information_schema.columns 
+                WHERE `table_schema` = '{$db}' 
+                AND `table_name` = '{$this->name}'
+                AND `column_name` = '{$name}';";
+        $result = $this->db->query($sql);
+        return $result->getResultArray()[0]['count'];
+    }
+
+    public function column($name, $type, $default = null) {
+        if(!$this->hasColumn($name)) {
+            $sql = "ALTER TABLE `{$this->name}` ADD `{$name}` {$type}";
+            if($default) $sql .= " DEFAULT {$default}";
+            $sql .= ";";
+            $this->db->query($sql);
+
+            ModelDefinitionCache::getInstance()->clearCache();
+        }
+        return $this;
+    }
+
+    public function dropTable() {
+        $sql = "DROP TABLE IF EXISTS ".$this->name;
+        $this->db->query($sql);
+        return $this;
+    }
+
+    public function truncate() {
+        $this->db->query("TRUNCATE {$this->name}");
+    }
+
+    public function dropColumn($name) {
+        $sql = "ALTER TABLE {$this->name} DROP COLUMN {$name};";
+        $this->db->query($sql);
+        return $this;
+    }
+
+    public function timestamps() {
+        return $this
+            ->column('created', ColumnTypes::DATETIME)
+            ->column('updated', ColumnTypes::DATETIME);
+    }
+
+    public function softDelete() {
+        return $this
+            ->column('deletion_id', ColumnTypes::INT);
+    }
+
+    public function createdUpdatedBy() {
+        return $this
+            ->column('created_by_id', ColumnTypes::INT)
+            ->column('updated_by_id', ColumnTypes::INT);
+    }
+
+    public function addIndex($names) {
+        $args = func_get_args();
+        $name = $args[0];
+
+        $indexes = [];
+        if(count($args) > 1)
+            $indexes = array_slice($args, 1);
+
+        if(count($indexes) > 0)
+            $indexes = implode(', ', $indexes);
+        else
+            $indexes = $name;
+
+        if($this->hasIndex($name))
+            Data::debug("Index $name already exists in {$this->name}");
+        else {
+            $this->db->query("ALTER TABLE `{$this->name}` ADD INDEX `$name` ($indexes)");
+            Data::debug("Index $name added to {$this->name}");
+        }
+
+        return $this;
+    }
+
+    public function hasIndex($name) {
+        if(!$this->hasColumn($name))
+            Data::debug(get_class($this), "column", $name, 'does not exist on table', $this->name);
+        $sql = "SELECT TABLE_SCHEMA, COUNT(1) IndexIsThere FROM INFORMATION_SCHEMA.STATISTICS
+                WHERE table_schema=DATABASE() AND table_name='{$this->name}' AND index_name='$name';";
+        $result = $this->db->query($sql);
+        $data = null;
+        foreach($result->getResult() as $row) {
+            if(strcmp($row->TABLE_SCHEMA, $this->db->getDatabase()) === 0)
+                $data = $row;
+        }
+        return isset($data) ? $data->IndexIsThere : false;
+    }
+}
