9390a626961c011cc58504287e1cedca389cbad1
martin <martin@4spaces.dk>
2023-01-13 16:04:48 +0100

    Added command to clear cache

---
+++ Commands/ClearCache.php
@@ -0,0 +1,36 @@
+<?php namespace OrmExtension\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+use CodeIgniter\Config\Config;
+use OrmExtension\DataMapper\ModelDefinitionCache;
+
+/**
+ * Class ClearCache
+ * @package RestExtension\Commands
+ */
+class ClearCache extends BaseCommand {
+
+    public $group           = 'OrmExtension';
+    public $name            = 'orm:clear:cache';
+    public $description     = 'Clear cache';
+    protected $usage        = 'orm:clear:cache';
+    protected $arguments    = [
+
+    ];
+    protected $options      = [
+
+    ];
+
+    /**
+     * Actually execute a command.
+     * This has to be over-ridden in any concrete implementation.
+     *
+     * @param array $params
+     * @throws \ReflectionException
+     */
+    public function run(array $params) {
+        ModelDefinitionCache::getInstance()->clearCache();
+    }
+
+}
